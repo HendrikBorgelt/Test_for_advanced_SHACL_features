1ee413a0ac299948fc3872e7af30beb62b21f659
HendrikBorgelt <hendrikborgelt@googlemail.com>
2024-08-12 12:48:47 +0200

    added some further tests

--- test_for_complex_query2.py
+++ test_for_complex_query2.py
@@ -1,9 +1,15 @@
 import rdflib
 
 g = rdflib.Graph()
-g.parse(".\\Ontology_tests\\data_for_query3.ttl", format="turtle")
-with open(".\\Ontology_tests\\knownQuery8.ttl", "r") as file:
+# g.parse(".\\Ontology_tests\\data_for_query3.ttl", format="turtle")
+g.parse(".\\shacl_sparql_test_data.ttl", format="turtle")
+
+# with open(".\\Ontology_tests\\knownQuery8.ttl", "r") as file:
+#     known_query1 = file.read()
+
+with open(".\\shacl_sparql_test_shape.ttl", "r") as file:
     known_query1 = file.read()
+
 
 qres = g.query(known_query1)
 
